60b42ff969023fd159032fe9e51169c475452c13
Thomas George <tfjgeorge@gmail.com>
2016-08-18 15:38:21 -0400

    bugfix

--- models/simple_mlp.py
+++ models/simple_mlp.py
@@ -36,7 +36,7 @@ def build_mlp(features_car_cat, features_car_int, features_nocar_cat, features_n
     prediction = mlp_nocar.apply(feature_nocar)
     # gating with the last feature : does the dude own a car
     prediction += tensor.addbroadcast(features_hascar, 1) * mlp_car.apply(feature_car)
-    prediction += tensor.addbroadcast(means[features_cp, None], 1)
+    prediction += tensor.addbroadcast(means[features_cp], 1)
 
     cost = MAPECost().apply(prediction, labels)
 
